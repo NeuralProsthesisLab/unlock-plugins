76b925e505a13e514f94d02d1a625db6061d290c
bgalbraith <byron.galbraith@gmail.com>
2015-10-27 01:25:14 -0400

    switch to (n_channel, n_sample) orientation

--- plugins/drivers/functionsignal.py
+++ plugins/drivers/functionsignal.py
@@ -6,7 +6,7 @@ from plugins.drivers.daqplugin import DAQPlugin
 class FunctionSignal(DAQPlugin):
     def __init__(self):
         super(FunctionSignal, self).__init__()
-        self.n_channels = 1
+        self.n_channels = 4
         self.t = 0
 
         def cycle(t):
@@ -14,14 +14,18 @@ class FunctionSignal(DAQPlugin):
                 return np.random.choice([1, 2, 3, 4, 5])
             else:
                 return 0
-        self.fn = cycle
+
+        def random(t):
+            return np.random.random(self.n_channels)
+
+        self.fn = random  # cycle
 
     def acquire(self):
         return 1
 
     def get_data(self, samples):
-        buffer = np.zeros((samples, self.n_channels))
+        buffer = np.zeros((self.n_channels, samples))
         for i in range(samples):
-            buffer[i] = self.fn(self.t)
+            buffer[:, i] = self.fn(self.t)
             self.t += 1
         return buffer
